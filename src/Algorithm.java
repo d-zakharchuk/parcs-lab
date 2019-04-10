20b329bc3c415fe6dff38f9801a9f5bdc01f6f44
d-zakharchuk <dima.97.2012@gmail.com>
2019-04-10 21:23:22 +0300

    Update Algorithm.java

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -2,11 +2,49 @@ import parcs.*;
 import java.math.BigInteger;
 
 public class Algorithm implements AM {
+
+public static BigInteger sqrt(BigInteger x) {
+    BigInteger div = BigInteger.ZERO.setBit(x.bitLength()/2);
+    BigInteger div2 = div;
+    // Loop until we hit the same value twice in a row, or wind
+    // up alternating.
+    for(;;) {
+        BigInteger y = div.add(x.divide(div)).shiftRight(1);
+        if (y.equals(div) || y.equals(div2))
+            return y;
+        div2 = div;
+        div = y;
+    }
+} 
+	
+	
+	public BigInteger FermatFactor(BigInteger N)
+    {
+        BigInteger a = sqrt(N);
+        BigInteger b2 = a.multiply(a).subtract(N);
+        while (!isSquare(b2))
+        {
+            a = a.add(ONE);
+	    b2 = a.multiply(a).subtract(N);
+        }
+        res1 = a.subtract(sqrt(b2));
+        return res1;
+        //display(r1, r2);
+    }
+    public boolean isSquare(BigInteger N)
+    {
+        BigInteger sqr = sqrt(N);
+	if (sqr.multiply(sqr).equals(N) || (sqr.add(ONE)).multiply(sqr.add(ONE)).equals(N))
+            return true;
+        return false;
+    }
 	
     public void run(AMInfo info) {
+    BigInteger N = (BigInteger) info.parent.readObject();
     BigInteger res1 = new BigInteger("0");
     BigInteger res2 = new BigInteger("0");
-    BigInteger N = (BigInteger) info.parent.readObject();
+    res1 = FermatFactor(N);
+    res2 = N.divide(res1);
     point p = info.createPoint();
     channel c = p.createChannel();
     p.execute("Algorithm");
