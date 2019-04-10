d4067bf76a7b3c6d97351e157c2ce0fabff0eb5c
d-zakharchuk <dima.97.2012@gmail.com>
2019-04-10 14:36:30 +0300

    Update Algorithm.java

--- src/Algorithm.java
+++ src/Algorithm.java
@@ -1,35 +1,17 @@
 import parcs.*;
+import java.math.BigInteger;
 
 public class Algorithm implements AM {
-	static String swap(String str, int i, int j){
-        char ch;
-        char[] array = str.toCharArray();
-        ch = array[i];
-        array[i] = array[j];
-        array[j] = ch;
-        return String.valueOf(array);
-    }
-    static void permute(String str,int low,int high, String result){
-        if(low == high)
-			result += str + "\n";
-            System.out.println(str);
- 
-        int i;
-        for(i = low; i<=high; i++){
-            str = swap(str,low,i);
-            permute(str, low+1,high, result);
-            str = swap(str,low,i);
-        }
-    }
 	
     public void run(AMInfo info) {
-    String result = new String("");
-    String N = (String) info.parent.readObject();
-    permute(N, 0, N.length()-1, result);
+    BigInteger res1 = new BigInteger("0");
+    BigInteger res2 = new BigInteger("0");
+    BigInteger N = (BigInteger) info.parent.readObject();
     point p = info.createPoint();
     channel c = p.createChannel();
     p.execute("Algorithm");
     c.write(N);
-    info.parent.write(result);
+    info.parent.write(res1);
+    info.parent.write(res2);
     }
 }
